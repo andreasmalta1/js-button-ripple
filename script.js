6464dc5cf15033a3722ba520ae8a0e5a383bfad2
Andreas Calleja <38852990+andreasmalta1@users.noreply.github.com>
2022-08-04 14:28:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const btnElement = document.querySelector('.btn')
+
+btnElement.addEventListener('mouseover', (event) => {
+    const y = event.pageY - btnElement.offsetlTop
+    const x = event.pageX - btnElement.offsetLeft
+
+    btnElement.style.setProperty("--xPos", x + "px")
+    btnElement.style.setProperty("--yPos", y + "px")
+})
